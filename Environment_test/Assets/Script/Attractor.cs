7ea4ea015b91212c8d92f0bc85d5ea114b793759
Terry <zty_24460@outlook.com>
2020-01-29 08:38:56 +0000

    Initial commit

---
+++ Environment_test/Assets/Script/Attractor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    public const float G = 6.674f;
+
+    public static List<Attractor> Attractors;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        foreach (Attractor attractor in Attractors)
+        {
+            if (attractor != this)
+                Attract(attractor);
+        }
+    }
+
+    void OnEnable()
+    {
+        if (Attractors == null)
+            Attractors = new List<Attractor>();
+
+        Attractors.Add(this);
+    }
+
+    void OnDisable()
+    {
+        Attractors.Remove(this);
+    }
+
+    void Attract(Attractor objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        if (distance == 0f)
+            return;
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Autonomous-Asteroid-Exploration-by-Rational-Agents-UoL-FYPZ:\Documents\Unity3D\Autonomous-Asteroid-Exploration-by-Rational-Agents-UoL-FYP\Unity Projects\Testing
+ 
