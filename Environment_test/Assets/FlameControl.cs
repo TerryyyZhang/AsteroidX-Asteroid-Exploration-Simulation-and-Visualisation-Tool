389d3652cb8d87a5f6b8a9058b9b7173699c8468
Terry <zty_24460@outlook.com>
2020-01-29 23:09:30 +0000

    Manual Mouse Control Feature with Thruster Flame

---
+++ Environment_test/Assets/FlameControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameControl : MonoBehaviour
+{
+    private Light flame;
+    ParticleSystem particle;
+    ShipInput SI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flame = GetComponentInChildren<Light>();
+        particle = GetComponent<ParticleSystem>();
+        SI = GetComponentInParent<ShipInput>();
+        flame.enabled = false;
+        particle.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SI.throttle > 0) { flame.enabled = true; particle.Play(); }
+        else { flame.enabled = false; particle.Stop(); }
+    }
+}
